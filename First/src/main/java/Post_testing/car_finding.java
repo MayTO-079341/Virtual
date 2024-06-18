81d409b9d085fe1e7947f6cbdeecc9923837800e
MayTO-079341 <oceanheart.thandar@gmail.com>
2024-06-19 00:48:47 +0630

    Add files via upload

--- First/src/main/java/Post_testing/car_finding.java
+++ First/src/main/java/Post_testing/car_finding.java
@@ -11,6 +11,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.time.Duration;
 
 public class car_finding {
@@ -29,29 +30,42 @@ public class car_finding {
 
     }
 
-    @Test(enabled = true)
+    @Test
     public void FindACar() throws InterruptedException, AWTException {
-
-/*        driver.get("https://carshare.yomafleet.com/account/signin");
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("input[id='username']")).sendKeys("maythandaroo720@gmail.com");
-        driver.findElement(By.id("password")).sendKeys("Thandar@test2");
-        driver.findElement(By.xpath("(//div[@class='cursor-pointer'])[1]")).click();
-        driver.findElement(By.cssSelector("button[type='submit']")).click();
-        Thread.sleep(2000);*/
 
         driver.findElement(By.xpath("//a[contains(@class,'text-textBlack hover:text-primary p-2 rounded-md text-sm font-medium font-source-serif uppercase dark:text-dOnSurface dark:hover:text-secondary')][text()='Find A Car']")).click();
         Thread.sleep(1000);
 
-//        driver.findElement(By.xpath("//div[@class='select__value-container css-hlgwow']")).sendKeys("yangon");
         WebElement dropdown = driver.findElement(By.xpath("//div[@class='select__indicators css-1wy0on6']"));
         JavascriptExecutor executor = (JavascriptExecutor) driver;
         executor.executeScript("arguments[0].click();", dropdown);
         dropdown.sendKeys("Yangon Airport (Coach Parking) , Mingalardon Township");
 //        Select city = new Select(dropdown);
 //        city.selectByVisibleText("Yangon Airport (Coach Parking) , Mingalardon Township");
+        Robot k = new Robot();
+        k.keyPress(KeyEvent.VK_ENTER);
         Thread.sleep(3000);
 
+        //pickup date&time
+//        driver.findElement(By.className("numInput cur-year")).sendKeys("2024");
+//        driver.findElement(By.xpath("//option[@value='5']")).click();
+        driver.findElement(By.cssSelector("span[class='flatpickr-day today selected']")).click(); // today date selected
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("input[placeholder='Select Pick-up Time']")).click();
+        Thread.sleep(2000);
+
+        //return date&time
+        driver.findElement(By.className("numInput cur-year")).sendKeys("2024");
+        driver.findElement(By.xpath("//option[@value='5']")).click();
+        driver.findElement(By.cssSelector("span[class='flatpickr-day selected']")).click(); // any date select
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("(//input[@class='numInput flatpickr-hour'])[2]")).sendKeys("9");
+        driver.findElement(By.xpath("(//input[@class='numInput flatpickr-minute'])[2]")).sendKeys("00");
+        Thread.sleep(2000);
+
+        //Click Find A Car button
+        driver.findElement(By.xpath("//button[@id='SimulateButton']")).click();
+        Thread.sleep(2000);
 
     }
 
