99bcb784d6841321a30b5852bbfc2af35115c989
MayTO-079341 <oceanheart.thandar@gmail.com>
2024-06-19 00:49:45 +0630

    Add files via upload

--- First/src/main/java/Post_testing/registration.java
+++ First/src/main/java/Post_testing/registration.java
@@ -68,7 +68,7 @@ public class registration {
 
     }
 
-    @Test(enabled = false)
+    @Test(enabled = true)
     public void Login() throws InterruptedException, AWTException {
 
         driver.get("https://carshare.yomafleet.com/account/signin");
@@ -79,70 +79,7 @@ public class registration {
         driver.findElement(By.cssSelector("button[type='submit']")).click();
         Thread.sleep(2000);
 
-        //Fill personal info
-        driver.findElement(By.xpath("(//button[@type='button'][text()='Upload'])[1]")).click();
-        //select image manually after clicking Upload
-        Thread.sleep(4000);
-        ///enter phone number
-        driver.findElement(By.xpath("(//div[@class='select__input-container css-15ol6m4'])[1]")).sendKeys("MM");
 
-        Robot k = new Robot();
-        k.keyPress(KeyEvent.VK_ENTER);
-        Thread.sleep(1000);
-        driver.findElement(By.id("national_number")).sendKeys("95126420");
-
-        ///select DOB
-        driver.findElement(By.className("numInput cur-year")).sendKeys("1999");
-        driver.findElement(By.xpath("//option[@value='0']")).click();
-        driver.findElement(By.cssSelector("span[aria-label='January 16, 1999']")).click();
-
-        ///choose country
-        driver.findElement(By.xpath("(//div[@class='select__input-container css-15ol6m4'])[2]")).sendKeys("myanmar");
-        k.keyPress(KeyEvent.VK_ENTER);
-
-        ///input city
-        driver.findElement(By.id("city")).sendKeys("Yangon");
-
-        ///input township
-        driver.findElement(By.id("township")).sendKeys("Latha");
-
-        ///input address
-        driver.findElement(By.id("address")).sendKeys("No.11, 9th street");
-
-        ///upload ID selfie
-        driver.findElement(By.xpath("(//button[@type='button'][text()='Upload'])[2]")).click();
-        //select image manually after clicking Upload
-        Thread.sleep(4000);
-
-        ///upload proof of address
-        driver.findElement(By.xpath("(//button[@type='button'][text()='Upload'])[3]")).click();
-        //select image manually after clicking Upload
-        Thread.sleep(4000);
-
-        //Select ID type
-
-        driver.findElement(By.xpath("(//div[@class='select__indicator select__dropdown-indicator css-1xc3v61-indicatorContainer'])[1]")).click();
-        k.keyPress(KeyEvent.VK_ENTER);
-        Thread.sleep(3000);
-
-        //fill NRC value
-        driver.findElement(By.xpath("(//div[@class='select__input-container css-15ol6m4'])[2]")).sendKeys("14");
-        k.keyPress(KeyEvent.VK_ENTER);
-        driver.findElement(By.xpath("(//div[@class='select__input-container css-15ol6m4'])[3]")).sendKeys("AhGaPa");
-        k.keyPress(KeyEvent.VK_ENTER);
-        driver.findElement(By.xpath("(//div[@class='select__input-container css-15ol6m4'])[4]")).sendKeys("N");
-        k.keyPress(KeyEvent.VK_ENTER);
-        driver.findElement(By.cssSelector("input[id='id_number']")).sendKeys("039282");
-        Thread.sleep(2000);
-
-        ///upload NRC image
-        driver.findElement(By.xpath("(//button[@type='button'][text()='Upload'])[1]")).click();
-        //select image manually after clicking Upload
-        Thread.sleep(4000);
-        ///upload NRC image
-        driver.findElement(By.xpath("(//button[@type='button'][text()='Upload'])[2]")).click();
-        //select image manually after clicking Upload
-        Thread.sleep(4000);
     }
 
     @AfterClass
